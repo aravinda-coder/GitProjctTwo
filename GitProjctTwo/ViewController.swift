a9d392f8c691c5de06a98b211cc3e5a9d1f90936
aravinda-coder <aravindabandara84@gmail.com>
2023-05-20 16:15:14 +0530

    commit is available

--- GitProjctTwo/ViewController.swift
+++ GitProjctTwo/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     //here is second change for the project
     }
 
 
