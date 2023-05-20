b3a88d340f80d07f1b849599753430c8e313bb99
aravinda-coder <aravindabandara84@gmail.com>
2023-05-20 16:16:39 +0530

    here the push code lines

--- GitProjctTwo/ViewController.swift
+++ GitProjctTwo/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
      //here is second change for the project
+        //another dule changes for the repo 
     }
 
 
